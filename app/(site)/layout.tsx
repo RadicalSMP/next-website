8ec71229d3b9325c70aa90b687bff17eac7d4b9e
hami <sweethami@qq.com>
2026-01-25 17:14:51 +0800

    feat: 更新目录结构, 便于制作前后台不同的layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
 import { Footer } from "@/components/ui/footer";
 import { ThemeProvider } from "@/components/theme-provider";
